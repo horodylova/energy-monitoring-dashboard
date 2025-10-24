00b44b7631ea53ac89ad241116b4e04fab6022ac
Svitlana Horodylova <horodylova.sv@gmail.com>
2025-10-24 16:21:20 +0100

    Fix build errors - simplify page component

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', 
+  images: {
+    domains: [],
+    formats: ['image/avif', 'image/webp'],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    });
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
