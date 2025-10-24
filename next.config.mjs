ad8337734b0381af71f0d67a27c98f2572441a9f
Svitlana Horodylova <horodylova.sv@gmail.com>
2025-10-24 15:45:44 +0100

    Fix React compatibility issue with Kendo UI - downgrade to React 18

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: [],
-    formats: ['image/avif', 'image/webp'],
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack']
-    });
-    return config;
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
 
